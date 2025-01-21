668c4ee30c273778895d6192ce5231c8b0b39302
Aqsa Arshad <160477492+Aqsaarshi@users.noreply.github.com>
2025-01-22 03:39:47 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tfm4zkfe";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 const token = process.env.NEXT_PUBLIC_SANITY_API_TOKEN;
 const apiVersion = '2025-01-17'; // Replace with your API version
 
