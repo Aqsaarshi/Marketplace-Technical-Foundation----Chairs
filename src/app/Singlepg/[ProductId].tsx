804e2911d83644bd2dd51c40aca0da20d8655801
Aqsa Arshad <160477492+Aqsaarshi@users.noreply.github.com>
2025-01-22 05:25:05 +0500

    Update [ProductId].tsx

--- src/app/Singlepg/[ProductId].tsx
+++ src/app/Singlepg/[ProductId].tsx
@@ -12,8 +12,9 @@ import Footer from "../Footer/Footer";
 
 const builder = imageUrlBuilder(client);
 
-const urlFor = (source: Product["image"]["asset"]) =>
-  builder.image(source).url();
+const urlFor = (source: any) => builder.image(source).url();
+
+
 
 interface Product {
   type: string;
